1322701b001d4a167e14fa034fc5486c5e450c5c
Dzhangir Rakhmatullin <developer4.lingplay@gmail.com>
2016-12-14 18:05:31 +0500

    Добавление плеера на сцену и рефакторинг.

---
+++ Assets/Scripts/ShowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPlayer : MonoBehaviour, IGvrGazeResponder {
+	void IGvrGazeResponder.OnGazeEnter() {
+		print("Enter");
+	}
+
+	void IGvrGazeResponder.OnGazeExit() {
+		print("Exit");
+	}
+
+	void IGvrGazeResponder.OnGazeTrigger() {
+		print("Trigger");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
